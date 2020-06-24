8d4004a1aacdaac6f3cec3a6185517a33f20c92a
Idalis Paz <idalispaz@gmail.com>
2020-06-24 21:23:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+attendees = ["Edsger", "Ada","Charles","Alan","Grace","Linus","Matz"]
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(array)
+    new_array = []
+    array.each do |name|
+    new_array << ("Hello, my name is #{name}.")
+  end
+  return new_array
+end
+  
+def assign_rooms(speakers)
+    new_array = []
+    speakers.each_each_index do |speakers, index|
+    number = index + 1
+    room_attendees_array = 
+    room_attendees_array << ("Hello, #{name}! You'll be assigned to room #{number}!")
+  end 
+    return room_array
+end 
+
+  
